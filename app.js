5ef69d0f4fca7551f54b7b8e3ec89f89e5dc25e8
alipinski15 <58602252+alipinski15@users.noreply.github.com>
2020-05-17 19:41:17 -0700

    Update

--- app.js
+++ app.js
@@ -47,6 +47,13 @@ app.use((req, res, next) => {
     next(err);
 })
 
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error', err);
+
+})
+
 //Handle the localhost server.
 
 app.listen(3000, () => {
